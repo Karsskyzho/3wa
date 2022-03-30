ce84553510bc1c7dcb86d88c3a86a981d8de95b8
karim_carsoule <karim.carsoule01@gmail.com>
2022-03-30 13:57:27 +0200

    push

--- class_exo/main.js
+++ class_exo/main.js
@@ -1,18 +1,11 @@
 import{User} from "./user.js";
 import {Admin} from "./admin.js";
 
-
 const main = () => {
     const user = new User(); // créer un objet de la classe User
-    const fullNamejjjj = user.fullName("John", "Doe"); // appeler la méthode fullName
-    console.log(fullNamejjjj);// afficher John Doe
-
-
-
-    const admin = new Admin(); // créer un objet de la classe Admin
-    const fullName = admin.fullName("John", "Doe");// appeler la méthode fullName
-    console.log(fullName);
-    console.log(admin.canEditArticles());
+    user.setfirstName("Francis");
+    user.setlastName("Dupont");
+    console.log(user.getFullName());
 };
 
-addEventListener('load', main);// lancer la fonction main au chargement de la page
+addEventListener('load', main);
--- class_exo/user.js
+++ class_exo/user.js
@@ -1,6 +1,35 @@
 export class User {
-    fullName(name, firstName) {
-    
-        return name + " " + firstName;
-}
+    constructor(firstName, lastName, job = 'bien_joué_chacal') {
+    this.job = job;
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+  
+  setfirstName(firstName) {
+    this.firstName = `${firstName}`;
+  }
+  getfirstName() {
+    return this.firstName;
+  }
+
+
+  setlastName(lastName) {
+    this.lastName = `${lastName}`;
+  }
+  getlastName() {
+    return this.lastName;
+  }
+
+
+  setjob(job) {
+    this.job = `${job}`;
+  }
+  getjob() {
+    return this.job;
+  }
+
+
+  getFullName() {
+    return this.firstName + ' ' + this.lastName;
+  }
 }
