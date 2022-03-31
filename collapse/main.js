8ab04ebeaa79aebcbe3edee5b875d9de62bc55d7
karim_carsoule <karim.carsoule01@gmail.com>
2022-03-31 16:22:19 +0200

    push

---
+++ collapse/main.js
@@ -0,0 +1,17 @@
+const main = () => {
+    
+    let table = [];
+    
+    
+    const container = document.getElementById('container');
+    const first = container
+    console.log(first);
+addEventListener(onclick, open)
+
+const open = () => {
+    first.classList.add('open');
+    
+}
+}
+
+addEventListener('load', main);
