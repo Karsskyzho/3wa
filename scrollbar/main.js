57f27754856c3941596d164a0dab897c97a5ed55
karim_carsoule <karim.carsoule01@gmail.com>
2022-03-31 14:24:27 +0200

    push

---
+++ scrollbar/main.js
@@ -0,0 +1,18 @@
+const main = () => {
+
+    const header = document.querySelector('.header');
+    const div = document.header.createElement('div');
+    div.className = 'loader';//присваиваем класс для лоадера
+    header.append(div);//добавляем лоадер в конец хедера
+    
+}
+
+window.addEventListener("scroll",);
+
+
+window.addEventListener('scroll', handleScroll);
+
+
+
+
+addEventListener('load', main)
